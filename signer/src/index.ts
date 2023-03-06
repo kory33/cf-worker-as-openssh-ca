ada63179517d753bfc06cdaf5e33996ff6153827
Ryosuke <korygm33@gmail.com>
2023-03-06 04:15:20 +0000

    refactor: tab to spaces

--- signer/src/index.ts
+++ signer/src/index.ts
@@ -13,37 +13,37 @@ import * as routes from "./routes";
  */
 
 export interface Env {
-	/**
-	 * The KV namespace in which a signing key pair should be stored.
-	 */
-	SIGNING_KEY_PAIR_NAMESPACE: KVNamespace;
+  /**
+   * The KV namespace in which a signing key pair should be stored.
+   */
+  SIGNING_KEY_PAIR_NAMESPACE: KVNamespace;
 
-	/**
-	 * The Service that is able to verify a request and return a list of principals.
-	 * 
-	 * The service bound to this variable MUST return a 200 response with a JSON body
-	 * that contains a single array containing the "principal names" for which
-	 * the request is valid. If the request cannot be authenticated, an empty array MUST
-	 * be returned.
-	 */
-	AUTHENTICATOR_SERVICE: Fetcher;
+  /**
+   * The Service that is able to verify a request and return a list of principals.
+   * 
+   * The service bound to this variable MUST return a 200 response with a JSON body
+   * that contains a single array containing the "principal names" for which
+   * the request is valid. If the request cannot be authenticated, an empty array MUST
+   * be returned.
+   */
+  AUTHENTICATOR_SERVICE: Fetcher;
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		const url = new URL(request.url);
-		const adapted = adapt(env);
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    const adapted = adapt(env);
 
-		if (request.method === "GET" && url.pathname === "/ca-public-key") {
-			return await routes.getCaPublicKey(request, adapted, ctx);
-		} else if (request.method === "POST" && url.pathname === "/new-short-lived-certificate") {
-			return await routes.postNewShortLivedCertificate(request, adapted, ctx);
-		} else {
-			return new Response(null, { status: 404 });
-		}
-	},
+    if (request.method === "GET" && url.pathname === "/ca-public-key") {
+      return await routes.getCaPublicKey(request, adapted, ctx);
+    } else if (request.method === "POST" && url.pathname === "/new-short-lived-certificate") {
+      return await routes.postNewShortLivedCertificate(request, adapted, ctx);
+    } else {
+      return new Response(null, { status: 404 });
+    }
+  },
 };
