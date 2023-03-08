71d48247f07fb72d236d76c5c09fa3e40ba3254d
Ryosuke <korygm33@gmail.com>
2023-03-08 00:01:52 +0000

    fix: rename RawEd25519Keys' field

--- signer/internal-crypto-wasm/src/exported_types.rs
+++ signer/internal-crypto-wasm/src/exported_types.rs
@@ -9,28 +9,28 @@ use wasm_bindgen::prelude::wasm_bindgen;
 #[wasm_bindgen]
 pub struct RawEd25519Keys {
     // must have length = 32 on input boundaries or panics
-    public: Box<[u8]>,
+    public_key: Box<[u8]>,
 
     // must have length = 32 on input boundaries or panics
-    unencrypted_private: Box<[u8]>,
+    unencrypted_private_key: Box<[u8]>,
 }
 
 #[wasm_bindgen]
 impl RawEd25519Keys {
     #[wasm_bindgen(constructor)]
-    pub fn new(public: Box<[u8]>, unencrypted_private: Box<[u8]>) -> Self {
+    pub fn new(public_key: Box<[u8]>, unencrypted_private_key: Box<[u8]>) -> Self {
         RawEd25519Keys {
-            public,
-            unencrypted_private,
+            public_key,
+            unencrypted_private_key,
         }
     }
 
     pub fn get_public(&self) -> Box<[u8]> {
-        self.public.clone()
+        self.public_key.clone()
     }
 
     pub fn get_unencrypted_private(&self) -> Box<[u8]> {
-        self.unencrypted_private.clone()
+        self.unencrypted_private_key.clone()
     }
 }
 
@@ -39,8 +39,8 @@ impl TryInto<Ed25519Keypair> for &RawEd25519Keys {
 
     fn try_into(self) -> Result<Ed25519Keypair, Self::Error> {
         Ok(Ed25519Keypair {
-            public: Ed25519PublicKey::try_from(self.public.as_ref())?,
-            private: Ed25519PrivateKey::try_from(self.unencrypted_private.as_ref())?,
+            public: Ed25519PublicKey::try_from(self.public_key.as_ref())?,
+            private: Ed25519PrivateKey::try_from(self.unencrypted_private_key.as_ref())?,
         })
     }
 }
@@ -48,8 +48,8 @@ impl TryInto<Ed25519Keypair> for &RawEd25519Keys {
 impl From<Ed25519Keypair> for RawEd25519Keys {
     fn from(value: Ed25519Keypair) -> Self {
         RawEd25519Keys {
-            public: Box::new(value.public.0),
-            unencrypted_private: Box::new(value.private.to_bytes()),
+            public_key: Box::new(value.public.0),
+            unencrypted_private_key: Box::new(value.private.to_bytes()),
         }
     }
 }
